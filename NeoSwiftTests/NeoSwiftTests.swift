acfef9ee4a69800cb629e67517f3287eea0dcab7
SGK Tech <sgktech@sgks-mac-mini.local>
2017-06-07 22:54:18 +0530

    Initial Commit

---
+++ NeoSwiftTests/NeoSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  NeoSwiftTests.swift
+//  NeoSwiftTests
+//
+//  Created by SGK Tech on 07/06/17.
+//  Copyright © 2017 SgkTech. All rights reserved.
+//
+
+import XCTest
+@testable import NeoSwift
+
+class NeoSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
